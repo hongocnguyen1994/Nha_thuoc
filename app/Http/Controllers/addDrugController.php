7a4e7e3fd44cfa6f97f6c445ef039ab21fd2bbd1
hongocnguyen1994 <hongocnguyen1994@gmail.com>
2017-11-17 14:12:25 +0700

    edit composer, backend, front-end

--- app/Http/Controllers/addDrugController.php
+++ app/Http/Controllers/addDrugController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\drugs;
 
 class addDrugController extends Controller
 {
     public function submit(){
-    	return 123;
+    	$drug = new drugs;
+    	
     }
 }
